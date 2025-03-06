d69d653d8e073bbc19ce5471fc11d36cf2c616c8
Cordova Villar Mario Alberto <mariocv670@gmail.com>
2025-03-06 09:26:09 -0600

    ejercicio de practica 4

--- ejemplo2.c
+++ ejemplo2.c
@@ -5,16 +5,19 @@ int main(){
     int *arreglo, num, cont;
     printf("¿Cuántos elementos tiene el conjunto?\n");
     scanf("%d", &num);
-    arreglo = (int*)calloc(num , sizeof(int));
+    printf("%p\n", arreglo);
+
+    arreglo = (int*)calloc(num , sizeof(int));   //primer elemento: el numero de elemntos, segundo: el tamaño de uno de los elementos
     if(arreglo != NULL){
         printf
         ("Vector reservado:\n\t[");
         for(cont =0; cont <num; cont++){
-            printf("\t%d", *(arreglo + cont));            
+            printf("\t%d, %p", *(arreglo + cont), (arreglo+cont));            
         }
         printf("\t]\n");
         printf("Liberando el espacio reservado\n");
-        free(arreglo);        
+        free(arreglo);    
+        arreglo = NULL    
     }
     return 0;
 }
